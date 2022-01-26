5bef3b58e0a79ae67a06d2233cf523fb7353e33c
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2022-01-25 23:16:15 -0600

    Add the Get() snippet function use to our showSnippet() handler function to get snippets from the database and display them

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/Gabriel0110/Golang-Practice/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -57,9 +60,19 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the fmt.Fprintf() function to interpolate the id value with our response
-	// and write it to the http.ResponseWriter
-	fmt.Fprintf(w, "Display a specific snippet with id %d...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body
+	fmt.Fprintf(w, "%v", s)
+
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
