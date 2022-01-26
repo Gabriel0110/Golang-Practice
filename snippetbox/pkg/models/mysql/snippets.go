7195bc811c4a7e41ac37dded933ba45b537b4a77
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2022-01-25 23:08:05 -0600

    Add implementation of Get() function to the snippet model in snippets.go

--- snippetbox/pkg/models/mysql/snippets.go
+++ snippetbox/pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/Gabriel0110/Golang-Practice/snippetbox/pkg/models"
 )
@@ -32,7 +33,40 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	statement := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(statement, id)
+
+	// init a pointer to a new zeroed snippet struct
+	s := &models.Snippet{}
+
+	// Use row.Scan() to copy the values from each field in sql.Row to the
+	// corresponding field in the Snippet struct. Notice that the arguments
+	// to row.Scan are *pointers* to the place you want to copy the data into,
+	// and the number of arguments must be exactly the same as the number of
+	// columns returned by your statement.
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		// If the query returns no rows, then row.Scan() will return a
+		// sql.ErrNoRows error. We use the errors.Is() function check for that
+		// error specifically, and return our own models.ErrNoRecord error
+		// instead.
+
+		// The reason we return models.ErrNoRecord error instead of sql.ErrNoRows directly
+		// is to help encapsulate the model completely, so that our application isn’t
+		// concerned with the underlying datastore or reliant on datastore-specific errors
+		// for its behavior.
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	// If everything went ok, then return the snippet object
+	return s, nil
+
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
