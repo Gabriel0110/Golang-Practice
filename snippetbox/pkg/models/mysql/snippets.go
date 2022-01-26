cb2b964dcce63e90bc2f8ea894f4a49198b06b65
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2022-01-25 21:48:12 -0600

    Implement the Insert function of the snippets model in the snippets.go file

--- snippetbox/pkg/models/mysql/snippets.go
+++ snippetbox/pkg/models/mysql/snippets.go
@@ -11,7 +11,24 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	statement := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(statement, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method (not available in all drivers) on the result object to get
+	// the ID of our newly inserted record in the snippets table
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int
+	// type before returning
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
